ecd3ec61e587736e992eedcfad6507cfceb531fa
Dennis Yiaile <ykamakei6345@gmail.com>
2021-06-19 03:17:36 +0300

    change the project details

--- js/scripts.js
+++ js/scripts.js
@@ -12,23 +12,49 @@ function Size(first, second, third, fourth,) {
 }
 $("#button").click(function(event){
   event.preventDefault();
-  var small=$("#s").val(500);
+  var name=$("#name").val();
+  var delivery=$("#delivery").val();
   var smallc=$("#sc").val();
   var smalls=$("#ss").val();
   var smallg=$("#sg").val();
-  var large=$("#l").val();
   var largec=$("#lc").val();
   var larges=$("#ls").val();
   var largeg=$("#lg").val();
-  var medium=$("#m").val();
   var mediumc=$("#mc").val();
   var mediums=$("#ms").val();
   var mediumg=$("#mg").val();
-  var select=[small, "#sc"];
-
-$("#story1").text(name);
-$("#story2").text(select[0]);
-$("#story3").text();
-$(".story").show();
+if (smallc && name) {
+  alert(name + ", your total bill is $2.04.");
+}
+else if (smalls && name) {
+  alert(name + ", your total bill is $2.40.");
+}
+else if (smallg && name) {
+  alert(name + ", your total bill is $2.21.");
+}
+else if (mediumc && name) {
+  alert(name + ", your total bill is $4.78.");
+}
+else if (mediums && name) {
+  alert(name + ", your total bill is $5.67.");
+}
+else if (mediumg && name) {
+  alert(name + ", your total bill is $5.73.");
+}
+else if (largec && name) {
+  alert(name + ", your total bill is $7.80.");
+}
+else if (larges && name) {
+  alert(name + ", your total bill is $7.56.");
+}
+else if (largeg && name) {
+  alert(name + ", your total bill is $8.41.");
+}
+else if (!name) {
+  alert("Please enter your name.");
+}
+else {
+  alert("Please enter the pizza size that you want.")
+}
 })
 })
